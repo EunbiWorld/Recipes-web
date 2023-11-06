2e5c82168d5eb917ede635f4e9e9bc3371357da5
EunbiWorld <rkql1304@naver.com>
2023-11-06 13:40:50 +0900

    파비콘 교체, 소개 페이지 슬라이드 사진 넣기.  스타일 수정

--- src/Component/Main/Introduction.jsx
+++ src/Component/Main/Introduction.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import IntroPage from "../../styled/IntroPage";
-import food1 from "../../res/img/food.jpeg";
-import food2 from "../../res/img/food.jpeg";
-import food3 from "../../res/img/food.jpeg";
+import food1 from "../../res/img/food1.png";
+import food2 from "../../res/img/food2.png";
+import food3 from "../../res/img/food3.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -18,23 +18,28 @@ const Introduction = () => {
 
   return (
     <IntroPage>
-      <div className="slide">
-        <Slider {...settings}>
-          <div className="slide-item">
-            <img src={food1} alt="Food 1" />
-            <div className="custom-dot" />
-          </div>
-          <div className="slide-item">
-            <img src={food2} alt="Food 2" />
-            <div className="custom-dot" />
-          </div>
-          <div className="slide-item">
-            <img src={food3} alt="Food 3" />
-            <div className="custom-dot" />
-          </div>
-        </Slider>
+      <div className="container">
+        <div className="slide">
+          <Slider {...settings}>
+            <div className="slide-item">
+              <img src={food1} alt="Food 1" />
+              <div className="custom-dot" />
+            </div>
+            <div className="slide-item">
+              <img src={food2} alt="Food 2" />
+              <div className="custom-dot" />
+            </div>
+            <div className="slide-item">
+              <img src={food3} alt="Food 3" />
+              <div className="custom-dot" />
+            </div>
+          </Slider>
+        </div>
+        <div className="content">
+          <h1>웹 페이지 소개</h1>
+          <p>내용 넣기</p>
+        </div>
       </div>
-      <div className="content"></div>
     </IntroPage>
   );
 };
--- src/styled/IntroPage.jsx
+++ src/styled/IntroPage.jsx
@@ -2,29 +2,27 @@ import styled from "styled-components";
 
 const IntroPage = styled.div`
   .slide {
+    overflow-x: hidden;
   }
 
   .slide img {
     max-width: 100%;
-    height: 50%;
   }
 
-.slick-dots {
+  .slick-dots {
     bottom: 2em;
-}
-.slick-dots li button:before {
+  }
+  .slick-dots li button:before {
     font-size: 20px;
     color: black;
-}
+  }
 
-.custom-dot {
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  /* 원하는 스타일 및 위치 조정을 수행하세요. */
-}
-
+  .custom-dot {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
 
 export default IntroPage;
