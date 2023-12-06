f37ba811424ee96e6f61022f82b39385ae5a4552
테스트 <saehun66@gmail.com>
2023-12-06 18:06:13 +0900

    추천 레시피 선택시 레시피 info로 넘어가게 수정

--- src/Component/Main/Recommendation.jsx
+++ src/Component/Main/Recommendation.jsx
@@ -27,8 +27,8 @@ const Recommendation = () => {
           <span>오늘의 추천 레시피입니다</span>
           <section className="recipe-grid">
             {recipes.map((recipe, index) => (
-              <div className="grid_item" key={recipe.ID}>
-                <Link to={`/Recipe/Info?id=${recipe.ID}`}>
+              <div className="grid_item" key={index}>
+                <Link to={`/RecipeList/Info/${index}`}>
                   <img src={recipe.Image} alt={recipe.Title} />
                   <div className={`recipe-title`}>
                     {recipe.Title}
